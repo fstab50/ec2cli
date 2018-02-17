1c023e9c1d3f6b9447028b6e2037f522b0c40528
Blake Huber <blake.huber@atos.net>
2018-02-17 14:19:13 -0600

    lib/colors.sh: updated to reference file

--- lib/colors.sh
+++ lib/colors.sh
@@ -10,14 +10,15 @@ white=$(tput setaf 7)
 yellow=$(tput setaf 3)
 orange='\033[38;5;95;38;5;214m'
 gray=$(tput setaf 008)
-lgray='\033[38;5;95;38;5;245m'    # light gray
-dgray='\033[38;5;95;38;5;8m'      # dark gray
+wgray='\033[38;5;95;38;5;251m'          # white-gray
+lgray='\033[38;5;95;38;5;245m'          # light gray
+dgray='\033[38;5;95;38;5;8m'            # dark gray
 reset=$(tput sgr0)
 
 # bright colors
 brightblue='\033[38;5;51m'
 brightcyan='\033[0;36m'
-brightgreen='\033[38;5;46m'
+brightgreen='\033[38;5;95;38;5;46m'
 brightyellow='\033[38;5;11m'
 brightyellow2='\033[38;5;95;38;5;226m'
 brightwhite='\033[38;5;15m'
@@ -27,13 +28,17 @@ bluepurple='\033[38;5;68m'
 title=$(echo -e ${underline})
 url=$(echo -e ${underline}${brightblue})
 options=$(echo -e ${white})
-commands=$(echo -e ${brightcyan})   # use for ansi escape color codes
+commands=$(echo -e ${brightcyan})       # use for ansi escape color codes
+frame=$(echo -e ${brightgreen})         # use for tables; border faming
+bodytext=$(echo -e ${reset${wgray}})    # main body text; set to reset for native xterm
 
 # initialize default color scheme
-accent=$(tput setaf 008)
-ansi=$(echo -e ${orange})   # use for ansi escape color codes
+accent=$(tput setaf 008)                # ansi format
+ansi_orange=$(echo -e ${orange})        # use for ansi escape color codes
 
 # accent
+bold='\u001b[1m'                        # ansi format
+underline='\u001b[4m'                   # ansi format
 BOLD=`tput bold`
 UNBOLD=`tput sgr0`
 
@@ -47,3 +52,4 @@ function indent04() { sed 's/^/    /'; }
 function indent10() { sed 's/^/          /'; }
 function indent15() { sed 's/^/               /'; }
 function indent18() { sed 's/^/                  /'; }
+function indent25() { sed 's/^/                         /'; }
