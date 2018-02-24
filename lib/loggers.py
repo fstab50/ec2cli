28bbe913293cf578f136a4472a1eea2448664388
Blake Huber <blake.huber@atos.net>
2018-02-24 15:38:12 -0600

    loggers.py: std logger added

---
+++ lib/loggers.py
@@ -0,0 +1,43 @@
+"""
+Summary:
+    Std Logging Module | FileHandler
+
+Returns:
+    logger object (logging)
+"""
+import os
+import logging
+
+
+# globals
+pkg = 'ec2cli'
+pkg_root = '/'.join(os.getcwd().split('/')[:-1])
+logfilename = pkg + '.log'
+log_path = pkg_root + '/' + log_filename
+
+# log format - file
+file_format = '%(asctime)s - %(pathname)s - %(name)s - [%(levelname)s]: %(message)s'
+asctime_format = "%Y-%m-%d %H:%M:%S"
+
+# objects
+logger = logging.getLogger(*args, **kwargs)
+logger.propagate = False
+
+
+def getLogger(*args, **kwargs):
+    """ std file handler logger """
+    #logger = logging.getLogger(*args, **kwargs)
+    #logger.propagate = False
+
+    try:
+        if not logger.handlers:
+            # file handler
+            f_handler = logging.FileHandler(log_path)
+            f_formatter = logging.Formatter(file_format, asctime_format)
+            f_handler.setFormatter(f_formatter)
+            logger.addHandler(f_handler)
+            logger.setLevel(logging.DEBUG)
+
+    except OSError as e:
+        raise e
+    return logger
