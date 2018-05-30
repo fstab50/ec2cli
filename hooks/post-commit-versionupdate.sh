a5f518a5bf0abb69972f4cf237db41167c4bd30d
Blake Huber <blake.huber@atos.net>
2018-05-30 14:35:48 -0500

    adds

---
+++ hooks/post-commit-versionupdate.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Post commit hook, updates version throughout project
+
+source core/_version.py
+
+VERSION="$__version__"
+DEPRECATED="$(grep 'Version:' README.md | awk -F ':' '{print $2}')"
+
+sed -i "s/$DEPRECATED/\t$VERSION/g" README.md
