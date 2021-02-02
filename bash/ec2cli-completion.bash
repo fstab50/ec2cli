eb9ee1b83deedafcea4a6b31784d9c8f829a7930
Blake Huber <blakeca00@gmail.com>
2021-02-01 21:22:47 -0600

    bash/ec2cli-completion.bash: change paths to /usr/local/lib/ec2cli

--- bash/ec2cli-completion.bash
+++ bash/ec2cli-completion.bash
@@ -169,7 +169,7 @@ function _ec2cli_completions(){
     local prev                      #  completion word at index position -1 in COMP_WORDS array
     local initcmd                   #  completion word at index position -2 in COMP_WORDS array
 
-    config_dir="$HOME/.config/ec2cli"
+    config_dir="/usr/local/lib/ec2cli"
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     initcmd="${COMP_WORDS[COMP_CWORD-2]}"
